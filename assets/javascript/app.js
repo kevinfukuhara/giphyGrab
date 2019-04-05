706aca454c90019d91b02f346963f13ef7d232f0
Fukuhara <Kevin.Fukuhara2@t-mobile.com>
2019-04-05 09:17:15 -0700

    Completed baseline logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,44 +3,24 @@
 
 // Initial array of movies
 var gifs = ["Finding Nemo", "Apex Legends", "Coding", "Sleepy"];
-
 var favorites = [];
-
 
 
 //  FUNCTIONS
 // ----------------------------
 
 // Function for displaying movie data
-function renderButtons() {
+function renderButtons(gifArr) {
     //Clear the movies buttons
     $("#btn-view").empty();
 
-    for (i = 0; i < gifs.length; i++) {
-        //   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs[i] + "&limit=10&rating=pg&api_key=dc6zaTOxFJmzC";
-
-        // var res;
-        //   $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        //   }).then(function (response) {
-        //     console.log(response);
-        // var newBtn = $("<button>");
-
-        // newBtn.attr("value", response.Title);
-        // newBtn.text(response.Title);
-
-        // $("#buttons-view").append(newBtn);
-
-        // $("button").on("click", function(){
-        //   console.log(this.value);
-        // });
-        console.log(gifs[i]);
+    for (i = 0; i < gifArr.length; i++) {
+        console.log(gifArr[i]);
 
         var newBtn = $("<button>");
 
-        newBtn.attr("value", gifs[i]);
-        newBtn.text(gifs[i]);
+        newBtn.attr("value", gifArr[i]);
+        newBtn.text(gifArr[i]);
 
         newBtn.addClass('gif-btn');
 
@@ -49,22 +29,110 @@ function renderButtons() {
 }
 
 function getGifs() {
+    event.preventDefault();
+
     console.log(this.getAttribute("value"));
     var searchTerm = this.getAttribute("value");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=10&rating=pg&api_key=dc6zaTOxFJmzC";
 
-    
+    // Creates AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // YOUR CODE GOES HERE!!!
+        console.log(response);      // Console shows that response.data returns and array!
+
+        // iterate through array
+        for (i = 0; i < response.data.length; i++) {
+            // <div class="card" style="width: 18rem;">
+            //     <img class="card-img-top" src="" alt="Card image cap" />
+            //     <div class="card-body">
+            //         <h5 class="card-title">Card title</h5>
+            //         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            //         <a href="#" class="btn btn-primary">Go somewhere</a>
+            //     </div>
+            // </div>
+
+            var cardMain = $("<div style='width: 18rem;'>");
+            cardMain.addClass("card");
+
+            // Gather Gif's main properties
+            gifStill = response.data[i].images.downsized_still.url;
+            gifMove = response.data[i].images.downsized.url;
+            gifRating = response.data[i].rating;
+            gifTitle = response.data[i].title;
+
+            var cardGif = $("<img>");
+            cardGif.addClass("card-img-top");
+            // Add gif's alt name/description
+            cardGif.attr("alt", gifTitle);
+            // Add gif's still value
+            cardGif.attr("still_value", gifStill);
+            // Add gif's active value
+            cardGif.attr("move_value", gifMove);
+            // Add the gif's src attribute to be the still value
+            cardGif.attr("src", gifStill);
+            
+            // Add identifier as image
+            cardGif.addClass("gif-image");
+
+            var cardBody = $("<div>");
+            // Add the rating of the gif
+            cardBody.html("Rating: " + gifRating);
+
+            // Append gif to Card Main, then append cardBody to cardMain
+            cardMain.append(cardBody);
+            cardMain.append(cardGif);
+            // Then append cardMain to gif-contain in the HTML 
+            $("#gif-contain").append(cardMain);
+
+            console.log("Should have added a new card/Gif to the gif-container.");
+        }
+
+    });
+
 }
 
 
 //  SETUP
 // ------------------------
-renderButtons();
+renderButtons(gifs);
 
 
 //  EVENT HANDLERS
 // -------------------------
-// $(document).on("click", ".gif-btn", getGifs($(this).attr("value")));
+
+// Handler controls the API and displaying of Gifs when a button on top is pressed
 $(document).on("click", ".gif-btn", getGifs);
 
+//  Event handler that handles the adding of a button
+$("#add-gif").on("click", function () {
+    // stop page from reloaded when button is pressed
+    event.preventDefault();
+
+    var gifType = $("#gif-input").val();
+
+    gifs.push(gifType);
+    renderButtons(gifs);
+});
+
+//Event handler that handles to play/pause gifs if clicked
+$(document).on("click", ".gif-image", function() {
+    // console.log($(this).attr("move_value"));
+    // stop page from reloaded when button is pressed
+    event.preventDefault();
+
+    var stillVal = $(this).attr("still_value");
+    var moveVal = $(this).attr("move_value");
+    var currState = $(this).attr("src");
+
+    if(currState === stillVal) {
+        // Case to Play Gif 
+        $(this).attr("src", moveVal);
+    } else if(currState === moveVal) {
+        // Case to pause gif
+        $(this).attr("src", stillVal);
+    }
+});
